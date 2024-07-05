0c3101afbc9ef9478677423175f055d07178acd9
QEdge <QEdge@DESKTOP-K1QIMKQ>
2024-07-05 12:08:26 +0530

    New Class Created with methods

---
+++ src/test/java/com/PageClasses/Demo.java
@@ -0,0 +1,25 @@
+package com.PageClasses;
+
+public class Demo {
+public void add()
+{
+	int a=34;
+	int b=56;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=787;
+	int b=78;
+	int c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=666;
+	int b=75;
+	int c=a+b;
+	System.out.println(c);
+}
+}
