d62114699c7c0645b7e56a21f6c8653779cce8a4
QEdge <QEdge@DESKTOP-K1QIMKQ>
2024-07-05 12:10:49 +0530

    Values Changed

--- src/test/java/com/PageClasses/Demo.java
+++ src/test/java/com/PageClasses/Demo.java
@@ -3,22 +3,22 @@ package com.PageClasses;
 public class Demo {
 public void add()
 {
-	int a=34;
-	int b=56;
+	int a=76576;
+	int b=76765;
 	int c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=787;
-	int b=78;
+	int a=7878;
+	int b=909;
 	int c=a+b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=666;
-	int b=75;
+	int a=5454;
+	int b=454;
 	int c=a+b;
 	System.out.println(c);
 }
